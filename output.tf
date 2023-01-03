748370b7162670bcc16aea9f7ddd7d651caab2cc
SREEHARI <68052722+sreehariskumar@users.noreply.github.com>
2023-01-03 19:14:28 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,11 +1,3 @@
-output "availability_zone" {
-  value = length(data.aws_availability_zones.available.names)
-}
-
-output "subnet_count" {
-  value = local.subnet_count
-}
-
 output "WordPress-URL" {
   value = "http://wordpress.${var.public_domain}"
 }
