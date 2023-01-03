0b6191ab3c6936c545f9b45507180a8ab4bbba14
SREEHARI <68052722+sreehariskumar@users.noreply.github.com>
2023-01-03 17:51:30 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,58 @@
+variable "project" {
+  default     = "zomato"
+  description = "project name"
+}
+
+variable "environment" {
+  default     = "production"
+  description = "project env"
+}
+
+variable "region" {
+  default     = "ap-south-1"
+  description = "project region"
+}
+
+variable "access_key" {
+  default = "XXXXXXXXXXXX"
+  description = project access key
+}
+
+variable "secret_key" {
+  default = "XXXXXXXXXXXX"
+  description = project secret key
+}
+
+variable "instance_ami" {
+  default = "ami-0cca134ec43cf708f"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+locals {
+  subnets = length(data.aws_availability_zones.available.names)
+}
+
+variable "vpc_cidr" {
+  default = "172.16.0.0/16"
+  description = cidr block to create vpc
+}
+
+locals {
+  common_tags = {
+    project     = var.project
+    environment = var.project
+  }
+}
+
+variable "private_domain" {
+  default = "sreehari.local"
+  description = "domain in private hosted zone"
+}
+
+variable "public_domain" {
+  default = "1by2.online"
+  description = "record to access wordpress website"
+}
